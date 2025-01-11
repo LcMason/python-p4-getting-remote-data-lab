2a70f7c82b92984616ed0c9b558f88663c49c77b
Lionel Mason <capitalcookhouse@gmail.com>
2025-01-11 08:37:09 -0500

    completes-getting-remote-data-lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,30 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        # URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(self.url)
+        return response.content
+
+#         # load_json method that should use get_response_body to send a request, then return a Python list or dictionary made up of data converted from the response of that request.
 
     def load_json(self):
         pass
+        # program_list = []
+        # programs = json.loads(self.get_response_body())
+        # for program in programs:
+        #     program_list.append(program.get("occuptation", "N/A)"))
+
+        # return program_list
+        data = json.loads(self.get_response_body())
+        return data
+
+
+# # In GetRequester.py
+if __name__ == "__main__":
+        requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+        
+        data = json.loads(requester.get_response_body())
+        print("Raw JSON data:", data)
+
+        processed_list = requester.load_json()
+        print("Processed list from load_json():", processed_list)
